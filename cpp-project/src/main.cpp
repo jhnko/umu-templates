cfbc8cd0ca388c5e936939661209a0d84247b08e
Jonathan Kostet <44286097+jhnko@users.noreply.github.com>
2025-09-04 17:19:03 +0200

    Enable many clang-tidy rules and use modern C++

--- cpp-project/src/main.cpp
+++ cpp-project/src/main.cpp
@@ -1,16 +1,19 @@
 #include <print>
 #include <vector>
 
-int main() {
-    std::println("Hello, World!");
+using std::println;
+using std::vector;
 
-    std::vector<int> list = {1, 2, 3};
+auto main() -> int {
+    println("Hello, World!");
+
+    vector<int> const list = {1, 2, 3};
 
     int sum = 0;
 
-    for (int num : list) {
+    for (int const num : list) {
         sum += num;
     }
 
-    std::println("The sum is {}", sum);
+    println("The sum is {}", sum);
 }
